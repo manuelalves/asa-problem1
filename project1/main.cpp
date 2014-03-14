9e13fa75046a97b6fdf6dea0c1b555fb48ede5ca
manuelalves <manuelalves@tecnico.ulisboa.pt>
2014-03-14 16:42:12 +0000

    input read

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,30 +1,71 @@
 #include <iostream>
+#include <list>
+#include <vector>
 
 using namespace std;
 
 int main(){
-    
+
     // number of people (n)
     int n = 0;
-    
+
     // number of shares (p)
     int p = 0;
-    
+
+    // share list
+    list<int> shareList;
+
+    // users vector
+    vector<list<int> > userVector;
+
+    // read first line input
     cin >> n;
     cin >> p;
-    
-    cout << n << " " << p << "\n";
+
+    // initialize users vector
+    for(int i = 0; i < n; i++){
+        userVector.push_back(shareList);
+    }
+
+    // insert shares on users vector
+    for (int j = 0; j < p; j++){
+        int auxN= 0;
+        int auxP = 0;
+
+        cin >> auxN;
+        cin >> auxP;
+
+        userVector[auxN-1].push_back(auxP);
+    }
+
+
+
+    // test print
+    /*for (int z = 0; z < n; z++){
+        list<int> test = userVector[z];
+        list<int>::const_iterator pos;
+
+        for(pos=test.begin(); pos != test.end(); ++pos){
+            cout << z + 1 << ' ' << *pos << ' ';
+        }
+        cout << "\n";
+    }*/
+
+
+    // write output
+    /*cout << n << " " << p << "\n";
     cout << n << "\n";
-    cout << p << "\n";
+    cout << p << "\n"; */
     
+
     return 0;
 }
 
 
-/* 
- 
+/*
+
  // Writing to a File
- 
+
  #include <fstream>
  ofstream outputFile;
  outputFile.open("output.txt");
