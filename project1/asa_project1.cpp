6757a16a849f9298bd8241cd185b71fdf59bd9bd
manuelalves <manuelalves@tecnico.ulisboa.pt>
2014-03-14 03:57:59 +0000

    input and output

--- project1/asa_project1.cpp
+++ project1/asa_project1.cpp
@@ -1,6 +1,37 @@
-#include<stdio.h>
+/* g++ -O3 -ansi -Wall asa_project1.cpp -lm */
 
- //Teste
+#include <iostream>
 
-/*jghjkfghkdhgkdghkjdhgjk*/
+using namespace std;
 
+int main(){
+    
+    // number of people (n)
+    int n = 0;
+    
+    // number of shares (p)
+    int p = 0;
+    
+    cin >> n;
+    cin >> p;
+    
+    cout << n << " " << p << "\n";
+    cout << n << "\n";
+    cout << p << "\n";
+    
+    return 0;
+}
+
+
+/* 
+ 
+ // Writing to a File
+ 
+ #include <fstream>
+ ofstream outputFile;
+ outputFile.open("output.txt");
+ outputFile << n << " " << p << endl;
+ outputFile << n << endl;
+ outputFile << p << endl;
+ outputFile.close();
+*/
