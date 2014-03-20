7c16ea6e151447d06c32aa376c0ffa6397936f34
jessicaribeiro <jessicafaaribeiro@gmail.com>
2014-03-20 23:46:00 +0000

    comentado

--- project1/main.cpp
+++ project1/main.cpp
@@ -5,6 +5,13 @@
 #include <stack>
 #include <stdio.h>
 
+/*
+[ASA]
+Grupo 108
+70179 - Manuel Alves
+70298 - Jessica Ribeiro
+*/
+
 using namespace std;
 
 class UserNode{
@@ -14,7 +21,7 @@ class UserNode{
           int low;
           bool L_exist;
           int visit;
-          list<int> sharedList;
+          list<int> sharedList;                             // List with the shared nodes
 };
 
 class Graph{
@@ -23,81 +30,82 @@ class Graph{
         int E;
         int visited;
         stack<int> L;
-        vector<list<int> > SCC;
-        vector<UserNode*> nodesVector;
+        vector<list<int> > SCC;                             // SCC list of strongly connected components
+        vector<UserNode*> nodesVector;                      // Vector that contais the graph nodes
 };
 
+
+// Tarjan Algorithm with DFS in each node of graph
 void Tarjan_Visit(int no, Graph* g){
 
-    UserNode* node = g->nodesVector[no-1];
+    UserNode* node = g->nodesVector[no-1];                  //gets the node in the nodesVector of graph
 
     node->d = g->visited;
     node->low = g->visited;
 
     g->visited = (g->visited + 1);
 
-    (g->L).push(node->id);
+    (g->L).push(node->id);                                  // add node to stack L
 
-    node->L_exist = true;
+    node->L_exist = true;                                   // node exists in the stack L
 
     list<int>::iterator adj;
 
-    // search the shared nodes of a node
-    for(adj = node->sharedList.begin(); adj!= node->sharedList.end(); adj++){
+
+    for(adj = node->sharedList.begin(); adj!= node->sharedList.end(); adj++){    // search the shared nodes of a node
 
         int adjNode = *(adj);
 
-        UserNode* shareNode = g->nodesVector[adjNode-1];
+        UserNode* shareNode = g->nodesVector[adjNode-1];   // gets the shared node
 
-        if(shareNode->d == -1){
-            Tarjan_Visit(shareNode->id, g);
-            node->low = min(shareNode->low, node->low);
+        if(shareNode->d == -1){                            // if the node was not visited yet
+            Tarjan_Visit(shareNode->id, g);                // runs Tarjan algorithm on shared node
+            node->low = min(shareNode->low, node->low);    // modify parents low to the minimun value
         }
-        else{
-            if(shareNode->L_exist == true){
-                node->low = min(node->low, shareNode->d);
+        else{                                              // the node was already been visited
+            if(shareNode->L_exist == true){                // if the shared node is still in the stack L
+                node->low = min(node->low, shareNode->d);  // modify parents low to the minimun value
             }
         }
     }
 
-    if(node->low == node->d){
+    if(node->low == node->d){                              // if we are in the root of the component
 
-        list<int> componentList;
+        list<int> componentList;                           // list of nodes contained in the component
         int w;
 
         do{
-            w = (g->L).top();
+            w = (g->L).top();                              // saves the first node of stack L
+            (g->L).pop();                                  // pop node from stack L
 
-            (g->L).pop();
+            UserNode* w_Node = g->nodesVector[w-1];        // gets the node from nodesVector of graph
 
-            UserNode* w_Node = g->nodesVector[w-1];
+            w_Node->L_exist = false;                       // node does not exist in the stack L
 
-            w_Node->L_exist = false;
+            componentList.push_back(w);                    // add node to the component
 
-            componentList.push_back(w);
+        }while(w != node->id);                             // while we are not in the root
 
-        }while(w != node->id);
-
-        g->SCC.push_back(componentList);
+        g->SCC.push_back(componentList);                   // /add the compoennt to the SCC list
     }
 }
 
 
-//returns the list with the SCC of a grafo
+//returns the list with the SCC of a graph
 vector<list<int> > SCC_Tarjan(Graph* g){
 
-    if(!(g->L).empty()){
+    if(!(g->L).empty()){                                   // empty stack L
         (g->L).pop();
     }
 
-    g->visited = 0;
+    g->visited = 0;                                        // number of nodes already visited in the graph
 
-    for (int node = 0; node < g->V; node++){
-        if(g->nodesVector[node]->d == -1){
-                Tarjan_Visit(node+1, g);
+    for (int node = 0; node < g->V; node++){               // for each node in the graph
+        if(g->nodesVector[node]->d == -1){                 // if was not visited yet (is undifined)
+                Tarjan_Visit(node+1, g);                   // apply Tarjan Algorithm to the node
         }
     }
-    return g->SCC;
+    return g->SCC;                                         // returns list of strongly connected components
 }
 
 
@@ -108,66 +116,65 @@ int main(){
     int number=0;
 
     // read first line input
-    scanf ("%d %d",&n,&p);
+    scanf ("%d %d",&n,&p);                                 // gets the number of Vertices and Edges of graph from iput
 
-    Graph* graph = new Graph();
+    Graph* graph = new Graph();                            // creates a new graph
 
-    graph->V = n;
-    graph->E = p;
-    graph->nodesVector.reserve(n);
+    graph->V = n;                                          // sets number of vertices of graph
+    graph->E = p;                                          // sets number of edges of graph
+    graph->nodesVector.reserve(n);                         // allocates a vector with number of vertices (number of nodes)
 
-    // initialize each node of the graph
-    for(int i = 0; i < graph->V; i++){
+    for(int i = 0; i < graph->V; i++){                     // initialize each node of the graph
 
-        UserNode* node = new UserNode();
+        UserNode* node = new UserNode();                   // creates a new node
 
-        graph->nodesVector.push_back(node);
+        graph->nodesVector.push_back(node);                // add node to the nodesVector of graph
 
         int id_aux = i;
 
-        graph->nodesVector[i]->id = (id_aux + 1);
-        graph->nodesVector[i]->d = -1;
-        graph->nodesVector[i]->low = -1;
+        graph->nodesVector[i]->id = (id_aux + 1);          // sets the id of node
+        graph->nodesVector[i]->d = -1;                     // sets the d of node
+        graph->nodesVector[i]->low = -1;                   // sets the low of node
     }
 
-    for (int j = 0; j < graph->E; j++){
+    for (int j = 0; j < graph->E; j++){                    // for each number of edges
 
         int user= 0;
         int share = 0;
 
-        scanf ("%d %d",&user, &share);
-        graph->nodesVector[user-1]->sharedList.push_back(share);
+        scanf ("%d %d",&user, &share);                     // gets the node and the shared_node
+        graph->nodesVector[user-1]->sharedList.push_back(share);   //adds shared_node to the sharedList of the node
     }
 
-    vector<list<int> > scComponents = SCC_Tarjan(graph);
+    vector<list<int> > scComponents = SCC_Tarjan(graph);   // aplly Tarjan Algorithm to the given graph
 
-    //tamanho do maior grupo maximo de pessoas que partilham informacao
+    //[2 - Tamanho do maior grupo maximo de pessoas que partilham informacao]
     int max_groups = 0;
 
-        for(unsigned int itr = 0; itr < scComponents.size(); itr++){
+        for(unsigned int itr = 0; itr < scComponents.size(); itr++){    // iterates the SSC vector
             list<int>::iterator adj;
 
             int max_groups_aux = 0;
 
-                max_groups_aux = scComponents[itr].size();
+                max_groups_aux = scComponents[itr].size();  // actualize max_group number to the biggest one finded
                 if(max_groups_aux > max_groups){
                     max_groups = max_groups_aux;
                 }
     }
 
 
-    //numero de grupos maximos de pessoas que partilham informacao apenas dentro do grupo
-    for(unsigned int itr = 0; itr < scComponents.size(); itr++){
+    //[3 - Numero de grupos maximos de pessoas que partilham informacao apenas dentro do grupo]
+    for(unsigned int itr = 0; itr < scComponents.size(); itr++){       // iterates the SSC vector
 
         list<int>::iterator adj;
 
         int sizee = scComponents[itr].size();
 
-        if(scComponents.size() == 1){
-            number ++;
+        if(scComponents.size() == 1){                                 // if the SCC component has only one node
+            number++;                                                 // increments number
         }else{
 
-            if(sizee == 1){
+            if(sizee == 1){                                           //
                 int a = scComponents[itr].front();
 
                 UserNode *node = graph->nodesVector[a - 1];
@@ -197,12 +204,12 @@ int main(){
     }
 
     //OUTPUT
-    //numero de grupos maximos de pessoas que partilham informacao
+    //[1 - Numero de grupos maximos de pessoas que partilham informacao]
     cout << scComponents.size() << "\n";
 
-    //tamanho do maior grupo maximo de pessoas que partilham informacao
+    //[2 - Tamanho do maior grupo maximo de pessoas que partilham informacao]
     cout << max_groups << "\n";
 
-    //numero de grupos maximos de pessoas que partilham informacao apenas dentro do grupo
+    //[3 - numero de grupos maximos de pessoas que partilham informacao apenas dentro do grupo]
     cout << number << "\n";
 }
